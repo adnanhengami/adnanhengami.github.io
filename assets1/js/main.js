a719c36b1b1e508db5c4200275e2a70df5609f4c
adnanhengami <adnanhengami400@gmail.com>
2025-08-07 05:28:39 -0700

    Add files via upload

---
+++ assets1/js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+  $(".pr-carousel").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    rtl: true,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 1008,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
